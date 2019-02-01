0d8361230fcb299934e76909bed77c24f85a87af
Suresh N <forsuresh@yandex.com>
2019-01-31 22:51:37 -0500

    code cleanup

--- OnTheMap/OnTheMap/Model/Parse Client/ParseClient.swift
+++ OnTheMap/OnTheMap/Model/Parse Client/ParseClient.swift
@@ -28,31 +28,11 @@ class ParseClient {
         }
     }
     
-//    class func taskForGetResponse(completion: @escaping (AllStudents?, Error?)-> Void){
-//
-//        let url = ParseClient.Endpoints.studentLocation.url
-//        var request = URLRequest(url: url)
-//
-//        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
-//        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
-//
-//
-//        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-//            guard let dataObject = data else {return}
-////            print(String(data: dataObject, encoding: .utf8)!)
-////            print("temp")
-//            do {
-//                let allStudentsObject = try JSONDecoder().decode(AllStudents.self, from: dataObject)
-//                    print(allStudentsObject)
-//                    completion(allStudentsObject, nil)
-//            } catch {
-//                DispatchQueue.main.async {
-//                    completion(nil, error)
-//                }
-//            }
-//        }
-//        task.resume()
-//    }
+    class func taskForGetResponse<ResponseType: Decodable>(request: URLRequest, responseObject: ResponseType.Type, completion: @escaping (ResponseType?, Error?)-> Void){
+    }
+    
+    
+    
     
     
     class func getAllStudents(completion: @escaping (AllStudents?, Error?)-> Void){
@@ -82,5 +62,32 @@ class ParseClient {
     
     
     
-    
 }
+
+
+/*
+ class func getAllStudents(completion: @escaping (AllStudents?, Error?)-> Void){
+ let url = ParseClient.Endpoints.studentLocation.url
+ var request = URLRequest(url: url)
+ 
+ request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+ request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+ 
+ 
+ let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+ guard let dataObject = data else {return}
+ //            print(String(data: dataObject, encoding: .utf8)!)
+ //            print("temp")
+ do {
+ let allStudentsObject = try JSONDecoder().decode(AllStudents.self, from: dataObject)
+ print(allStudentsObject)
+ completion(allStudentsObject, nil)
+ } catch {
+ DispatchQueue.main.async {
+ completion(nil, error)
+ }
+ }
+ }
+ task.resume()
+ }
+ */
